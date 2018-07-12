9357a582e5daf018dc9215a57d7f21362cd9565b
J772AHE <J772AHE@TNT06000091.nl.express.tnt>
2018-07-12 12:44:25 +0200

    Initial commit

---
+++ Test Suites/Data DrivenTest Suite.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Data DrivenTest Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-07-11T12:51:00</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>75a0302d-2eda-425e-837d-e9d2ffa3b02f</testSuiteGuid>
+   <testCaseLink>
+      <guid>e62a5236-bb37-455f-9a94-fa9bc3e86e10</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login Test</testCaseId>
+      <testDataLink>
+         <combinationType>MANY</combinationType>
+         <id>0152e34e-f6a9-4671-b4e5-a8266f8725d2</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/User Data</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>0152e34e-f6a9-4671-b4e5-a8266f8725d2</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Username</value>
+         <variableId>971760fe-52ab-47f2-a5e0-281de94e93a1</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>0152e34e-f6a9-4671-b4e5-a8266f8725d2</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Password</value>
+         <variableId>882452b2-4978-4f70-af0e-251ca5460610</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
